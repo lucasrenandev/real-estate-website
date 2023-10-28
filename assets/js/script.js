65a1d5c13ad9382c587e58d4439cb84cb8bf04ac
lucasrenandns <lucasrenandns96@gmail.com>
2023-10-27 23:44:58 -0300

    Update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,16 @@
 "use strict";
 
 const header = document.querySelector(".header");
+const menuIcon = document.querySelector(".header #menu-icon");
+const navBar = document.querySelector(".header .navbar");
 
 window.addEventListener("scroll", function() {
     header.classList.toggle("sticky", this.window.scrollY > 80);
+    navBar.classList.remove("open");
+    menuIcon.classList.remove("bx-x");
+});
+
+menuIcon.addEventListener("click", function() {
+    navBar.classList.toggle("open");
+    menuIcon.classList.toggle("bx-x");
 });
